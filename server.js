d3ba7c46f13a6bc33cd71bfaa22d6565e565f5eb
brentbess21 <brentbess21@gmail.com>
2021-09-30 13:19:08 -0600

    created both POST endpoints

--- server.js
+++ server.js
@@ -24,7 +24,24 @@ server.get('/api/users', (req, res)=> {
    res.status(200).json(users)
 })
 
+server.post('/api/register', (req, res)=> {
+    const newUser = { id: 3, username:"jack", password:"password3"}
+    req.body = newUser
+    res.status(201).json(newUser)
+})
 
+server.post('/api/login', (req, res)=> {
+    const { username, password } = req.body
+    if (!username || !password) {
+        res.status(404).json({
+            message: "credentials not found"
+        })
+    } else {
+        res.status(201).json({
+            message: "welcome!"
+        })
+    }
+})
 
 
 
